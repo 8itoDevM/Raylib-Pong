547f85e119e7c35aca666e3cd3a64d8c859fce6c
8ito <gustavomarcolino862@gmail.com>
2025-06-12 00:40:26 -0300

    Rendering objects

--- main.cpp
+++ main.cpp
@@ -1,17 +1,23 @@
 #include "raylib.h"
 
 int main(){
-    InitWindow(1280, 720, "Pong");
+    const int screen_width = 1280;
+    const int screen_height = 720;
+
+    InitWindow(screen_width, screen_height, "Pong");
     SetTargetFPS(60);
 
     // GameLoop
     while(WindowShouldClose() == false){
-        // 1. Event Handling
-
-        // 2. Updating Positions
-
-        // 3. Drawing
         BeginDrawing();
+
+        DrawLine(screen_width/2, 0, screen_width/2, screen_height, WHITE);
+
+        DrawCircle(screen_width/2, screen_height/2, 20, WHITE);
+        DrawRectangle(10, screen_height/2 - 60, 25, 120, WHITE);      
+        
+        DrawRectangle(screen_width - 35, screen_height/2 - 60, 25, 120, WHITE); 
+
         EndDrawing();
     }
 
