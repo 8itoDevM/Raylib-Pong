4193249c33b5877c2689d8ba4d4bb1d64b9b6cb5
8ito <gustavomarcolino862@gmail.com>
2025-06-12 00:53:09 -0300

    Implement ball movement and collision detection

--- main.cpp
+++ main.cpp
@@ -1,8 +1,40 @@
 #include "raylib.h"
 
+class Ball{
+public:
+    float x, y;
+    int speed_x, speed_y;
+    int radius;
+
+    void Draw(){
+        DrawCircle(x, y, radius, WHITE);
+    }
+
+    void Update(){
+        x += speed_x;
+        y += speed_y;
+
+        if(y + radius >= GetScreenHeight() || y - radius <= 0){
+            speed_y *= -1;
+        }
+
+        if(x + radius >= GetScreenWidth() || x - radius <= 0){
+            speed_x *= -1;
+        }
+    }
+};
+
+Ball ball;
+
 int main(){
     const int screen_width = 1280;
     const int screen_height = 720;
+
+    ball.radius = 20;
+    ball.x = screen_width/2;
+    ball.y = screen_height/2;
+    ball.speed_x = 7;
+    ball.speed_y = 7;
 
     InitWindow(screen_width, screen_height, "Pong");
     SetTargetFPS(60);
@@ -11,11 +43,16 @@ int main(){
     while(WindowShouldClose() == false){
         BeginDrawing();
 
+        // Updating
+        ball.Update();
+
+        ClearBackground(BLACK);
+
         DrawLine(screen_width/2, 0, screen_width/2, screen_height, WHITE);
 
-        DrawCircle(screen_width/2, screen_height/2, 20, WHITE);
+        ball.Draw();
+
         DrawRectangle(10, screen_height/2 - 60, 25, 120, WHITE);      
-        
         DrawRectangle(screen_width - 35, screen_height/2 - 60, 25, 120, WHITE); 
 
         EndDrawing();
